c7e495a075037b5949630f369bc32ce1946bcb85
aster <137767097+aster-void@users.noreply.github.com>
2025-03-28 13:18:44 +0900

    init stack

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { page } from "$app/state";
+  import Header from "+web/parts/Header.svelte";
+  import "../app.css";
+
+  const { children } = $props();
+</script>
+
+<svelte:head>
+  <meta property="og:site_name" content="SvelteKit project template" />
+  <meta property="og:url" content={page.url.toString()} />
+</svelte:head>
+
+<Header />
+{@render children()}
