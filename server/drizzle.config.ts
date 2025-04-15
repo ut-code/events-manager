3129f65d0b97b4b916319d9317611ec254d9d4f8
aster <137767097+aster-void@users.noreply.github.com>
2025-04-15 23:48:07 +0900

    init turso db

--- server/drizzle.config.ts
+++ server/drizzle.config.ts
@@ -1,12 +1,38 @@
 import { defineConfig } from "drizzle-kit";
+import { panic } from "./lib.ts";
+
+function env(name: string) {
+  return Bun.env[name] ?? panic(`Environment variable ${name} not found`);
+}
+
+const uniqueOptions: ReturnType<typeof defineConfig> = (() => {
+  switch (env("DB")) {
+    case "local":
+      return {
+        dialect: "sqlite",
+        dbCredentials: {
+          url: "file:../local.db",
+        },
+      };
+    case "turso":
+      return {
+        dialect: "turso",
+        dbCredentials: {
+          url: env("DATABASE_URL"),
+          authToken: env("DATABASE_AUTH_TOKEN"),
+        },
+      };
+    default:
+      return panic(
+        `Bun.env.DB does not match any known variable! got ${env("DB")}`,
+      );
+  }
+})();
 
 export default defineConfig({
-  dialect: "sqlite",
-  dbCredentials: {
-    url: "file:../local.db",
-  },
   schema: "./db/schema.ts",
   out: "./.drizzle",
   verbose: true,
   strict: true,
+  ...uniqueOptions,
 });
